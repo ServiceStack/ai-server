a5a15ff84210087896d46ce91b2c9804cb8d643b
Darren Reid <darren@reidmail.org>
2024-07-30 15:06:58 +1000

    Logging for download.

--- AiServer.ServiceInterface/Executor/DownloadComfyModelCommand.cs
+++ AiServer.ServiceInterface/Executor/DownloadComfyModelCommand.cs
@@ -1,6 +1,7 @@
 using AiServer.ServiceInterface.Comfy;
 using AiServer.ServiceModel;
 using AiServer.ServiceModel.Types;
+using Microsoft.Extensions.Logging;
 using ServiceStack;
 using ServiceStack.Data;
 
@@ -12,7 +13,7 @@ public class DownloadComfyModel
     public ComfyApiModel? DownloadModel { get; set; }
 }
 
-public class DownloadComfyModelCommand(AppConfig appConfig, IDbConnectionFactory dbFactory) : IAsyncCommand<DownloadComfyModel>
+public class DownloadComfyModelCommand(AppConfig appConfig, IDbConnectionFactory dbFactory, ILogger<DownloadComfyModelCommand> logger) : IAsyncCommand<DownloadComfyModel>
 {
     public async Task ExecuteAsync(DownloadComfyModel request)
     {
@@ -21,10 +22,20 @@ public class DownloadComfyModelCommand(AppConfig appConfig, IDbConnectionFactory
             var providerApiUrl = request.Provider.ApiBaseUrl;
             var comfyClient = new ComfyClient(providerApiUrl,request.Provider.ApiKey);
             var model = request.DownloadModel;
+            
+            // Check if download already in progress by looking at status
+            var downloadStatus = await comfyClient.GetDownloadStatusAsync(model.Filename);
+            if(downloadStatus.Progress > 0)
+            {
+                logger.LogWarning("Download already in progress for {Filename}, currently {Complete}% complete.", model.Filename, downloadStatus.Progress);
+                return;
+            }
             // If the model has civitai in model details, use the civitai api key
             ComfyAgentDownloadStatus res;
             if(model.Url.Contains("civitai"))
             {
+                logger.LogInformation("Starting download of {Filename} with CivitAI API key from {Url}.", 
+                    model.Filename, model.DownloadUrl);
                 res = await comfyClient.DownloadModelAsync(
                     request.DownloadModel.DownloadUrl,
                     request.DownloadModel.Filename,
@@ -34,6 +45,7 @@ public class DownloadComfyModelCommand(AppConfig appConfig, IDbConnectionFactory
             }
             else
             {
+                logger.LogInformation("Starting download of {Filename} from {Url}.", model.Filename, model.DownloadUrl);
                 res = await comfyClient.DownloadModelAsync(
                     request.DownloadModel.DownloadUrl,
                     request.DownloadModel.Filename);
