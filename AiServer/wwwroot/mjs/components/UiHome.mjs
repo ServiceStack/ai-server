bf9acb9201827752e09883e953ca3ea5b0c72011
Demis Bellot <demis.bellot@gmail.com>
2024-10-16 16:49:40 +0800

    Update UI to load screenshots

--- AiServer/wwwroot/mjs/components/UiHome.mjs
+++ AiServer/wwwroot/mjs/components/UiHome.mjs
@@ -202,7 +202,7 @@ export default {
                 
         <div class="mt-8 flow-root">
           <div class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-            <img src="https://tailwindui.com/plus/img/component-images/project-app-screenshot.png" alt="App screenshot" width="2432" height="1442" class="rounded-md shadow-2xl ring-1 ring-gray-900/10">
+            <img :src="'/img/uis/'+(routes.op || 'chat')+'.webp'" alt="App screenshot" width="2432" height="1442" class="rounded-md shadow-2xl ring-1 ring-gray-900/10">
           </div>
         </div>
       </div>
